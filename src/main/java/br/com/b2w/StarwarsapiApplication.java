e4f773a23609cadc7c21f3693df001185e248c53
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2018-11-02 19:26:42 -0200

    Iniciando Travis com CI e TDD

---
+++ src/main/java/br/com/b2w/StarwarsapiApplication.java
@@ -0,0 +1,12 @@
+package br.com.b2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarwarsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarwarsapiApplication.class, args);
+	}
+}
