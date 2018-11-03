6616227d64bd73ba0788699619a94ceba3b76cbd
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2018-11-03 00:58:34 -0200

    Criando testes de integração para serviço

---
+++ src/test/java/br/com/b2w/services/impl/PlanetServiceIntegrationTest.java
@@ -0,0 +1,47 @@
+package br.com.b2w.services.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PlanetServiceIntegrationTest {
+	
+	private PlanetService planetService;
+
+    @Before
+    public void setUp() throws Exception {
+        planetService = new PlanetService();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+
+    @Test
+    public void testFindById() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testFindByName() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetAll() {
+        fail("Not yet implemented");
+    }
+    
+    @Test
+    public void testCreate() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDelete() {
+        fail("Not yet implemented");
+    }
+}
