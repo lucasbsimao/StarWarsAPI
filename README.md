76d99a84d863e0e87e06aec331f34fbdc9466c0a
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2018-11-07 03:52:04 -0200

    Adicionando imagem

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 [![Build Status](https://travis-ci.org/lucasbsimao/purchaseapp.svg?branch=master)](https://travis-ci.org/lucasbsimao/StarWarsAPI)
 
+
 # StarWarsAPI
-API para pesquisa de planetas presentes em uma galáxia muito, muito distante...
+
+### 1. Conceito
+
+Esta API tem como propósito realizar buscas, inclusão e remoção de planetas do universo Star Wars. Estando de acordo com as informações do universo Star Wars original, a API retorna também o número de aparições nos filmes.
+
+### 2. Dependências
+
+Para executar este projeto são necessárias as seguintes dependências instaladas:
+
+* [Java SDK 8](https://www.oracle.com/technetwork/java/javaee/downloads/java-ee-sdk-downloads-3908423.html);
+* [Eclipse com Spring Tools Suite 3](https://www.eclipse.org/downloads/);
+* [Mongo DB](https://docs.mongodb.com/manual/installation/)
+* Todas as configurações para funcionamento correto da API são feitas pela aplicação, incluindo a criação do banco no Mongo DB;
+
+Há também a necessidade que o [SwAPI](https://swapi.co/) esteja online, do contrário a api não conseguirá acessá-lo.
+
+### 3. Testando a API
+
+A documentação padrão da aplicação é fornecida pelo [Swagger](https://swagger.io/). Assim, uma vez que a API estiver online, a documentação estará disponível no endpoint ``` /docApi/swagger-ui.html ```, como é visto abaixo:
+
+![Swagger](https://github.com/lucasbsimao/StarWarsAPI/blob/master/images/swagger.png "Swagger")
+
+
--- src/main/java/br/com/b2w/config/SwaggerConfig.java
+++ src/main/java/br/com/b2w/config/SwaggerConfig.java
@@ -6,8 +6,11 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -20,9 +23,20 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
         return new Docket(DocumentationType.SWAGGER_2)
         		.select()                                  
                 .apis(RequestHandlerSelectors.basePackage("br.com.b2w.controllers"))             
-                .paths(PathSelectors.any())                          
-                .build();    
+                .paths(PathSelectors.any())  
+                .build()
+                .apiInfo(metaData());    
              
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Star Wars REST API")
+                .description("\"Spring Boot REST API for search info in Star Wars universe\"")
+                .version("1.0.0")
+                .license("Apache License Version 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Lucas Borsatto", "https://github.com/lucasbsimao", "lucasborsattosimao@hotmail.com"))
+                .build();
     }
     
     @Override
